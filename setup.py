bde92945226925027b9cbceb7cde02648b9ad083
robbaronbu <robbaron@bu.edu>
2017-07-24 15:55:09 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Hello World',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+      author='Rob Baron',
+      author_email='robbaron@bu.edu',
+      url='',
+      install_requires=['Flask>=0.12.2', 'MarkupSafe'],
       )
